a5afa968c6fd949a5d1b154b148857836960ac1c
李铭昕 <715557344@qq.com>
2018-06-28 13:59:45 +0800

    增加单元测试

---
+++ test/Cases/HttpTest.php
@@ -0,0 +1,22 @@
+<?php
+// +----------------------------------------------------------------------
+// | OrmTest.php [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
+// +----------------------------------------------------------------------
+namespace Swoft\Test\Cases;
+
+use App\Models\Entity\User;
+
+class HttpTest extends AbstractTestCase
+{
+    public function testHttpIndex()
+    {
+        $res = $this->json('POST', '/');
+        $res = json_decode($res->getBody()->getContents(), true);
+
+        $this->assertEquals('Swoft Framework', $res['name']);
+    }
+}
