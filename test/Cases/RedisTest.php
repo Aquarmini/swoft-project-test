8f67a2ea81d7df153ff8723908e4badd85c0528c
李铭昕 <715557344@qq.com>
2018-07-02 09:46:20 +0800

    完善Redis基础测试

--- test/Cases/RedisTest.php
+++ test/Cases/RedisTest.php
@@ -9,9 +9,15 @@ class RedisTest extends AbstractTestCase
 {
     public function testRedisSet()
     {
-        /** @var Redis $res */
+        /** @var Redis $redis */
         $redis = bean(Redis::class);
         $res = $redis->set('swoft:string', 'xxx', 600);
         $this->assertTrue($res);
+
+        $res = $redis->get('swoft:string');
+        $this->assertEquals('xxx', $res);
+
+        $res = $redis->ttl('swoft:string');
+        $this->assertEquals(600, $res);
     }
 }
