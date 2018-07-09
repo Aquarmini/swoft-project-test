6ba09baab295b0fef3ec2e0c66dda7d0e6f6b5fe
李铭昕 <715557344@qq.com>
2018-07-09 16:20:08 +0800

    增加导出到csv测试

---
+++ test/Cases/Another/FileTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Swoft\Test\Cases\Another;
+
+use Swoft\Test\AbstractTestCase;
+
+class FileTest extends AbstractTestCase
+{
+    public function testExportCsv()
+    {
+        $csv_header = ['id', 'name'];
+        $csv_body = [
+            [1, 'limx'],
+            [2, 'Agnes'],
+        ];
+        $file = alias('@runtime') . '/test.csv';
+        // 打开文件资源，不存在则创建
+        $fp = fopen($file, 'a');
+        // 处理头部标题
+        $header = implode(',', $csv_header) . PHP_EOL;
+        // 处理内容
+        $content = '';
+        foreach ($csv_body as $k => $v) {
+            $content .= implode(',', $v) . PHP_EOL;
+        }
+        // 拼接
+        $csv = $header . $content;
+        // 写入并关闭资源
+        fwrite($fp, $csv);
+        fclose($fp);
+
+        $this->assertTrue(file_exists($file));
+    }
+}
