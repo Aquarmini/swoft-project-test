0292ca99052b725d2d75190ab3446ff1812a9ba8
李铭昕 <715557344@qq.com>
2018-07-09 12:13:29 +0800

    增加array_count_values测试

--- test/Cases/Another/ArrayTest.php
+++ test/Cases/Another/ArrayTest.php
@@ -35,4 +35,34 @@ class ArrayTest extends AbstractTestCase
         $this->assertTrue($array['t1'] === 0);
         $this->assertTrue($array['t2'] === 3);
     }
+
+    public function testArrayCountValues()
+    {
+        $arr = [1, 3, 3, '3', '4', 'a', 'b', 'a'];
+        $res = array_count_values($arr);
+        $this->assertEquals([
+            1 => 1,
+            3 => 3,
+            4 => 1,
+            'a' => 2,
+            'b' => 1
+        ], $res);
+
+        $arr2 = [1, '4', 'a', 'b'];
+        $res2 = array_count_values($arr2);
+        $this->assertEquals([
+            1 => 1,
+            4 => 1,
+            'a' => 1,
+            'b' => 1
+        ], $res2);
+
+        $res = array_intersect_key($res, $res2);
+        $this->assertEquals([
+            1 => 1,
+            4 => 1,
+            'a' => 2,
+            'b' => 1
+        ], $res);
+    }
 }
